4a19dfe6dbc35240343d69e9dd44cb43fd09a0ef
Koichi Yoshizaki <kyoshi@oita-u.ac.jp>
2024-07-07 04:30:34 +0000

    bcryptによるユーザ認証の開発

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_29_051627) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_07_041949) do
   create_table "comments", force: :cascade do |t|
     t.string "body"
     t.integer "post_id", null: false
@@ -24,6 +24,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_29_051627) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "signin_name"
+    t.string "display_name"
+    t.string "password_digest"
+    t.string "status", default: "available"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["signin_name"], name: "index_users_on_signin_name", unique: true
   end
 
   add_foreign_key "comments", "posts"
