34c7c37eb05195a818189c490094da7c8801c19e
Koichi Yoshizaki <kyoshi@oita-u.ac.jp>
2024-07-15 01:24:21 +0000

    ユーザ認証機能の完成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
  resources :posts do
   resources :comments
  end
+ resources :users
+ resources :sessions
  root 'posts#index'
 
  get "up" => "rails/health#show", as: :rails_health_check
